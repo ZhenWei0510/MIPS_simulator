4e9ef23e3dea837503fdd8692335c0bb7e57e5b4
Wei <a1105526@mail.nuk.deu.tw>
2023-12-28 18:34:55 +0800

    完成 branch (可能有 bug)

--- MIPS_SIMULATOR.cpp
+++ MIPS_SIMULATOR.cpp
@@ -81,6 +81,8 @@ struct ID_EX_pipeline {
     uint8_t rd;
 
     string op;
+
+    bool zero;
 } ID_EX, ID_tmp;
 
 struct EX_MEM_pipeline {
@@ -98,6 +100,10 @@ struct EX_MEM_pipeline {
     uint32_t Write_data;
     uint8_t Write_reg;
 
+    uint8_t rs;
+    uint8_t rt;
+    uint8_t rd;
+
     string op;
 } EX_MEM, EX_tmp;
 
@@ -112,6 +118,10 @@ struct MEM_WB_pipeline {
 
     uint8_t Write_reg;
 
+    uint8_t rs;
+    uint8_t rt;
+    uint8_t rd;
+
     string op;
 } MEM_WB, MEM_tmp;
 
@@ -124,8 +134,9 @@ Instruction ins_mem[INS_MEM_SIZE];  // instruction memory
 
 bool IF_IDWrite = true;
 bool PCWrite = true;
+bool BranchTaken = false;
 
-int cycle = 1;
+int cycle = 0;
 
 int main() {
     // 初始化 register
@@ -157,7 +168,7 @@ int main() {
     // }
 
     do {
-        cout << "Cycle " << cycle << endl;
+        cout << "Cycle " << ++cycle << endl;
         WB();
         MEM();
         EX();
@@ -165,11 +176,16 @@ int main() {
         IF();
         update_pipeline_register();
         cout << endl;
-
-        cycle++;
         if (cycle == 50) break;
     } while (!(ins_mem[PC].op == "" && IF_ID.ins.op == "" && ID_EX.op == "" && EX_MEM.op == "" && MEM_WB.op == ""));
-
+    cout << "Total cycles: " << cycle << endl << endl;
+    for (int i = 0; i < REG_NUM; i++) {
+        cout << "$" << i << " " << reg_file[i] << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < DATA_MEM_SIZE; i++) {
+        cout << "W" << i << " " << data_mem[i] << endl;
+    }
     return 0;
 }
 
@@ -232,25 +248,37 @@ int load_instructions() {
 }
 
 void IF() {
-    cout << "  IF: " << ins_mem[PC].op << endl;
+    cout << "   IF: " << ins_mem[PC].op << endl;
     IF_tmp.ins = ins_mem[PC];
     IF_tmp.PC = PC + 1;
     // cout << (int)IF_ID.ins.rs << " " << (int)IF_ID.ins.rt << " " << (int)IF_ID.ins.rd << endl;
 }
 
 void ID() {
-    cout << "  ID: " << IF_ID.ins.op << endl;
+    cout << "   ID: " << IF_ID.ins.op << endl;
+
+    // branch target
+    ID_tmp.PC = IF_ID.PC + IF_ID.ins.constant;
+
     // Load-use hazard detection
-    // cout << ID_EX.op << " " << ID_EX.MemRead << " " << (int)ID_EX.rt << " " << (int)IF_ID.ins.rs << " " << (int)IF_ID.ins.rt << endl;
     if (ID_EX.op != "" && ID_EX.MemRead == '1' && (ID_EX.rt == IF_ID.ins.rs || ID_EX.rt == IF_ID.ins.rt)) {
+        // insert bubble
         ID_tmp.RegDst = ID_tmp.ALUSrc = '0';
         ID_tmp.Branch = ID_tmp.MemRead = ID_tmp.MemWrite = '0';
         ID_tmp.RegWrite = ID_tmp.MemToReg = '0';
         ID_tmp.op = "";
+        // prevent update from PC and IF/ID pipeline register
         IF_IDWrite = PCWrite = false;
-        // cout << "test2";
         return;
     }
+
+    
+
+    ID_tmp.Read_data_1 = reg_file[IF_ID.ins.rs];
+    ID_tmp.Read_data_2 = reg_file[IF_ID.ins.rt];
+
+    ID_tmp.zero = ID_tmp.Read_data_1 == ID_tmp.Read_data_2 ? true :false;
+
     if (IF_ID.ins.op == "add" || IF_ID.ins.op == "sub") {
         ID_tmp.RegDst = '1';
         ID_tmp.ALUSrc = '0';
@@ -277,16 +305,29 @@ void ID() {
         ID_tmp.MemToReg = 'X';
     } else if (IF_ID.ins.op == "beq") {
         // Load-use hazard detection
-        // cout << ID_EX.op << " " << ID_EX.MemRead << " " << (int)ID_EX.rt << " " << (int)IF_ID.ins.rs << " " << (int)IF_ID.ins.rt << endl;
-        // cout << EX_MEM.op << " " << EX_MEM.MemRead << " " << (int)EX_MEM.Write_reg << " " << (int)IF_ID.ins.rs << " " << (int)IF_ID.ins.rt << endl;
         if ((ID_EX.op != "" && ID_EX.MemRead == '1' && (ID_EX.rt == IF_ID.ins.rs || ID_EX.rt == IF_ID.ins.rt)) || (EX_MEM.op != "" && EX_MEM.MemRead == '1' && (EX_MEM.Write_reg == IF_ID.ins.rs || EX_MEM.Write_reg == IF_ID.ins.rt))) {
             ID_tmp.RegDst = ID_tmp.ALUSrc = '0';
             ID_tmp.Branch = ID_tmp.MemRead = ID_tmp.MemWrite = '0';
             ID_tmp.RegWrite = ID_tmp.MemToReg = '0';
             ID_tmp.op = "";
             IF_IDWrite = PCWrite = false;
-            cout << "test1";
             return;
+        }
+        // 前前前指令為 lw
+        if (MEM_WB.op != "" && MEM_WB.MemToReg == '1') {
+            if (MEM_WB.Write_reg == IF_ID.ins.rs) {
+                ID_tmp.zero = MEM_WB.Read_data == ID_tmp.Read_data_1 ? true : false;
+            } else if (MEM_WB.Write_reg == IF_ID.ins.rt) {
+                ID_tmp.zero = MEM_WB.Read_data == ID_tmp.Read_data_2 ? true : false;
+            }
+        }
+        // 前前指令為 add/sub
+        if (EX_MEM.op != "" && EX_MEM.MemToReg == '0') {
+            if (EX_MEM.Write_reg == IF_ID.ins.rs) {
+                ID_tmp.zero = EX_MEM.ALU_result == ID_tmp.Read_data_1 ? true : false;
+            } else if (EX_MEM.Write_reg == IF_ID.ins.rt) {
+                ID_tmp.zero = EX_MEM.ALU_result == ID_tmp.Read_data_2 ? true : false;
+            }
         }
         ID_tmp.RegDst = 'X';
         ID_tmp.ALUSrc = '0';
@@ -295,20 +336,19 @@ void ID() {
         ID_tmp.MemWrite = '0';
         ID_tmp.RegWrite = '0';
         ID_tmp.MemToReg = 'X';
+    } else {
+        ID_tmp.RegDst = '0';
+        ID_tmp.ALUSrc = '0';
+        ID_tmp.Branch = '0';
+        ID_tmp.MemRead = '0';
+        ID_tmp.MemWrite = '0';
+        ID_tmp.RegWrite = '0';
+        ID_tmp.MemToReg = '0';
     }
-    // if (MEM_WB.RegWrite && (MEM_WB.Write_reg != 0)
-    //     && !(EX_MEM.RegWrite && (EX_MEM.Write_reg != 0)
-    //         && (EX_MEM.Write_reg == ID_EX.rs))
-    //     && (MEM_WB.Write_reg == ID_EX.rs)) {
-    //     // ForwardA = 01
-    // }
 
     IF_IDWrite = PCWrite = true;
 
-    ID_tmp.PC = IF_ID.PC;
-
-    ID_tmp.Read_data_1 = reg_file[IF_ID.ins.rs];
-    ID_tmp.Read_data_2 = reg_file[IF_ID.ins.rt];
+    // ID_tmp.PC = IF_ID.PC;
 
     ID_tmp.op = IF_ID.ins.op;
 
@@ -320,7 +360,13 @@ void ID() {
 }
 
 void EX() {
-    cout << "  EX: " << ID_EX.op << endl;
+    cout << "   EX: " << ID_EX.op;
+    if (ID_EX.op != "") {
+        cout << " " << ID_EX.RegDst << ID_EX.ALUSrc
+            << " " << ID_EX.Branch << ID_EX.MemRead << ID_EX.MemWrite
+            << " " << ID_EX.RegWrite << ID_EX.MemToReg;
+    }
+    cout << endl;
 
     EX_tmp.Branch = ID_EX.Branch;
     EX_tmp.MemRead = ID_EX.MemRead;
@@ -330,28 +376,37 @@ void EX() {
 
     EX_tmp.op = ID_EX.op;
 
-    EX_tmp.ALU_result = 123;
-    
-    // // ALUOp
-    // if (ID_EX.op == "add") {
-    //     EX_MEM.ALU_result = ID_EX.Read_data_1 + ID_EX.Read_data_2;
-    // } else if (ID_EX.op == "sub") {
-    //     EX_MEM.ALU_result = ID_EX.Read_data_1 - ID_EX.Read_data_2;
-    // } else if (ID_EX.op == "lw" || ID_EX.op == "sw") {
-    //     EX_MEM.ALU_result = ID_EX.Read_data_1 + ID_EX.offset;
-    // }
+    // EX_tmp.ALU_result = 123;
     
     // // ALU inputs
-    // uint32_t input_1 = ID_EX.Read_data_1;
-    // // Pass Read data 2 to Write_data
-    // EX_tmp.Write_data = ID_EX.Read_data_2;
+    uint32_t input_1 = ID_EX.Read_data_1;
+    // Pass Read data 2 to Write_data
+    EX_tmp.Write_data = ID_EX.Read_data_2;
 
+    // // ALUOp
+    if (ID_EX.op == "add") {
+        EX_tmp.ALU_result = ID_EX.Read_data_1 + ID_EX.Read_data_2;
+    } else if (ID_EX.op == "sub") {
+        EX_tmp.ALU_result = ID_EX.Read_data_1 - ID_EX.Read_data_2;
+    } else if (ID_EX.op == "lw" || ID_EX.op == "sw") {
+        EX_tmp.ALU_result = ID_EX.Read_data_1 + ID_EX.offset;
+    }
+    
     // RegDst mux to choose rd or rt
-    EX_MEM.Write_reg = ID_EX.RegDst == '0' ? ID_EX.rt : ID_EX.rd;
+    EX_tmp.Write_reg = ID_EX.RegDst == '0' ? ID_EX.rt : ID_EX.rd;
+
+    EX_tmp.rs = ID_EX.rs;
+    EX_tmp.rt = ID_EX.rt;
+    EX_tmp.rd = ID_EX.rd;
 }
 
 void MEM() {
-    cout << "  MEM: " << EX_MEM.op << endl;
+    cout << "  MEM: " << EX_MEM.op;
+    if (EX_MEM.op != "") {
+        cout << " " << EX_MEM.Branch << EX_MEM.MemRead << EX_MEM.MemWrite
+            << " " << EX_MEM.RegWrite << EX_MEM.MemToReg;
+    }
+    cout << endl;
 
     MEM_tmp.RegWrite = EX_MEM.RegWrite;
     MEM_tmp.MemToReg = EX_MEM.MemToReg;
@@ -360,9 +415,17 @@ void MEM() {
         // 執行讀取記憶體操作，將結果存入 MEM_WB 的 ALU_result
         MEM_tmp.Read_data = data_mem[EX_MEM.ALU_result];
     }
+    // cout << EX_MEM.op << " " << (int)EX_MEM.rs << " " << (int)EX_MEM.rt << " " << (int)EX_MEM.rd << endl;
+    // cout << MEM_WB.op << " " << (int)MEM_WB.rs << " " << (int)MEM_WB.rt << " " << (int)MEM_WB.rd << endl;
+    // cout << EX_MEM.ALU_result << endl;
     if (EX_MEM.MemWrite == '1') {
         // 執行寫入記憶體操作
-        data_mem[EX_MEM.ALU_result] = EX_MEM.Write_data;
+        cout << (int)MEM_WB.rd << " " << (int)EX_MEM.rt << endl;
+        if (MEM_WB.RegWrite == '1' && MEM_WB.rd == EX_MEM.rt) { // forwarding
+            data_mem[EX_MEM.ALU_result/4] = MEM_WB.ALU_result;
+        } else {
+            data_mem[EX_MEM.ALU_result/4] = EX_MEM.Write_data;
+        }
     } else {
         MEM_tmp.ALU_result = EX_MEM.ALU_result;
     }
@@ -370,10 +433,18 @@ void MEM() {
     MEM_tmp.Write_reg = EX_MEM.Write_reg;
 
     MEM_tmp.op = EX_MEM.op;
+
+    MEM_tmp.rs = EX_MEM.rs;
+    MEM_tmp.rt = EX_MEM.rt;
+    MEM_tmp.rd = EX_MEM.rd;
 }
 
 void WB() {
-    cout << "  WB: " << MEM_WB.op << endl;
+    cout << "   WB: " << MEM_WB.op;
+    if (MEM_WB.op != "") {
+        cout << " " << MEM_WB.RegWrite << MEM_WB.MemToReg;
+    }
+    cout << endl;
 
     if (MEM_WB.RegWrite == '1') {
         if (MEM_WB.MemToReg == '1') {
@@ -385,13 +456,6 @@ void WB() {
 }
 
 void update_pipeline_register() {
-    if (IF_IDWrite) {
-        IF_ID.ins = IF_tmp.ins;
-    }
-    if (PCWrite) {
-        PC = IF_tmp.PC;
-    }
-
     // ID_EX
     ID_EX.RegDst = ID_tmp.RegDst;
     ID_EX.ALUSrc = ID_tmp.ALUSrc;
@@ -411,6 +475,19 @@ void update_pipeline_register() {
     ID_EX.rt = ID_tmp.rt;
     ID_EX.rd = ID_tmp.rd;
     ID_EX.offset = ID_tmp.offset;
+    ID_EX.zero = ID_tmp.zero;
+
+    if (IF_IDWrite) {
+        IF_ID.ins = IF_tmp.ins;
+    }
+    if (PCWrite) {
+        if (ID_EX.zero && ID_EX.Branch == '1') {
+            IF_ID.ins.op = "";
+            PC = ID_EX.PC;
+        } else {
+            IF_ID.PC = PC = IF_tmp.PC;
+        }
+    }
 
     // EX_MEM
     EX_MEM.Branch = EX_tmp.Branch;
@@ -425,6 +502,10 @@ void update_pipeline_register() {
     EX_MEM.Write_data = EX_tmp.Write_data;
     //
     EX_MEM.Write_reg = EX_tmp.Write_reg;
+    //
+    EX_MEM.rs = EX_tmp.rs;
+    EX_MEM.rt = EX_tmp.rt;
+    EX_MEM.rd = EX_tmp.rd;
 
     // MEM_WB
     MEM_WB.RegWrite = MEM_tmp.RegWrite;
@@ -433,4 +514,10 @@ void update_pipeline_register() {
     MEM_WB.ALU_result = MEM_tmp.ALU_result;
     MEM_WB.Write_reg = MEM_tmp.Write_reg;
     MEM_WB.op = MEM_tmp.op;
+    //
+    MEM_WB.rs = MEM_tmp.rs;
+    MEM_WB.rt = MEM_tmp.rt;
+    MEM_WB.rd = MEM_tmp.rd;
 }
+
+// 解決: 1, 2, 3
